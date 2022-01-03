f9dfd13b5bef02ee646d1602874c5fa68c610949
Daniel Müllenborn <damuellen@aol.com>
2022-01-03 02:38:51 +0000

    System Package Dependency

--- Package.swift
+++ Package.swift
@@ -3,36 +3,39 @@
 
 import PackageDescription
 
+#if os(Windows)
+let targets: [PackageDescription.Target] = [
+  .target(name: "xlsxwriter", dependencies: ["Cxlsxwriter"], linkerSettings: [.linkedLibrary("zlibstatic.lib")]), 
+  .target(name: "Cxlsxwriter", dependencies: ["Cmd5", "Ctmpfileplus", "Cminizip"]),
+  .target(name: "Cminizip"),
+  .target(name: "Ctmpfileplus"),
+  .target(name: "Cmd5")
+]
+targets.filter { $0.name.hasPrefix("C") }.forEach { $0.cxxSettings = [.define("_CRT_SECURE_NO_WARNINGS")] }
+#else
+let targets: [PackageDescription.Target] = [
+  .target(name: "xlsxwriter", dependencies: ["Cxlsxwriter"]), 
+  .testTarget(name: "xlsxwriterTests", dependencies: ["xlsxwriter"]),
+]
+#endif
+
+#if os(Linux)
+let dependencies: [PackageDescription.Package.Dependency] = [
+  .package(name: "Cxlsxwriter", url: "https://github.com/damuellen/Cxlsxwriter.git", .branch("linux"))
+]
+#elseif os(macOS)
+let dependencies: [PackageDescription.Package.Dependency] = [
+  .package(name: "Cxlsxwriter", url: "https://github.com/damuellen/Cxlsxwriter.git", .branch("mac"))
+]
+#else
+let dependencies: [PackageDescription.Package.Dependency] = []
+#endif
+
 let package = Package(
   name: "xlsxwriter.swift",
   products: [
-    .library(name: "Cminizip", targets: ["Cminizip"]),
     .library(name: "xlsxwriter", targets: ["xlsxwriter"]),
-  ],
-  dependencies: [],
-  targets: [
-    .target(name: "xlsxwriter", dependencies: ["Cxlsxwriter"]), 
-    .target(name: "Cxlsxwriter", dependencies: ["Cmd5", "Ctmpfileplus", "Cminizip"]),
-    .target(name: "Cminizip"),
-    .target(name: "Ctmpfileplus"), .target(name: "Cmd5"),
-    .testTarget(name: "xlsxwriterTests", dependencies: ["xlsxwriter"]),
-  ]
+  ], 
+  dependencies: dependencies,
+  targets: targets
 )
-
-if let xlsxwriter = package.targets.first(where: { $0.name == "Cxlsxwriter" }) {
-  #if os(Windows)
-  xlsxwriter.linkerSettings = [.linkedLibrary("zlibstatic.lib")]
-  package.targets.filter { $0.name.hasPrefix("C") }
-    .forEach { $0.cxxSettings = [.define("_CRT_SECURE_NO_WARNINGS")] }
-  #else
-  xlsxwriter.linkerSettings = [.linkedLibrary("z")]
-  #endif
-}
-
-if let minizip = package.targets.first(where: { $0.name == "Cminizip" }) {
-  #if os(Windows)
-  minizip.linkerSettings = [.linkedLibrary("zlibstatic.lib")]
-  #else
-  minizip.linkerSettings = [.linkedLibrary("z")]
-  #endif
-}
