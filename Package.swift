620a2a54cda425be3ccb5c2eeca2e52d719c771a
Daniel Müllenborn <damuellen@aol.com>
2022-01-06 23:13:36 +0000

    5.0

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
