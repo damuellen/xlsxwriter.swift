4f27b80b8055afc3afbbef3cbdff32158cb7b2b1
Daniel Müllenborn <damuellen@aol.com>
2022-02-12 16:30:23 +0100

    Update xlsxwriter.h

--- Sources/Cxlsxwriter/xlsxwriter.h
+++ Sources/Cxlsxwriter/xlsxwriter.h
@@ -1,5 +1,5 @@
 #ifdef _WIN32
-#include "C:/vcpkg/installed/x64-windows/include/xlsxwriter.h"
+#include "C:/vcpkg/installed/x64-windows-static/include/xlsxwriter.h"
 #elif defined(__APPLE__) && defined(__aarch64__)
 #include "/opt/homebrew/include/xlsxwriter.h"
 #else
