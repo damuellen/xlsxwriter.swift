25e1c3dc359e4f050cc5a7a286e758e887971ba1
Daniel Müllenborn <damuellen@aol.com>
2022-08-12 16:00:54 +0200

    Update Cell_Range.swift

--- Sources/xlsxwriter/Cell_Range.swift
+++ Sources/xlsxwriter/Cell_Range.swift
@@ -34,7 +34,7 @@ public struct Cols: ExpressibleByStringLiteral, ExpressibleByArrayLiteral {
     self.col2 = UInt16(elements[1])
   }
   init(_ col: UInt16, _ col2: UInt16) {
-    self.col = col2
+    self.col = col
     self.col2 = col2
   }
 }
